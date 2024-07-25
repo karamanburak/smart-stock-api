97a5ba60216c21d754c64dccb29043731c6bd2b0
Burak Karaman <karaman.buraak@gmail.com>
2024-07-25 09:22:56 +0200

    Remove swagger definitions, and update db connection and swagger autogen files

--- swaggerAutogen.js
+++ swaggerAutogen.js
@@ -42,13 +42,13 @@ const document = {
   security: [{ Token: [] }, { Bearer: [] }],
   definitions: {
     // Models:
-    User: require("./src/models/user").schema.obj,
-    Brand: require("./src/models/brand").schema.obj,
-    Category: require("./src/models/category").schema.obj,
-    Firm: require("./src/models/firm").schema.obj,
-    Product: require("./src/models/product").schema.obj,
-    Purchase: require("./src/models/purchase").schema.obj,
-    Sale: require("./src/models/sale").schema.obj,
+    // User: require("./src/models/user").schema.obj,
+    // Brand: require("./src/models/brand").schema.obj,
+    // Category: require("./src/models/category").schema.obj,
+    // Firm: require("./src/models/firm").schema.obj,
+    // Product: require("./src/models/product").schema.obj,
+    // Purchase: require("./src/models/purchase").schema.obj,
+    // Sale: require("./src/models/sale").schema.obj,
   },
 };
 
